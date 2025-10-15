323e8dc8554ea876b62ef954baa13d6fbb649923
Nikica <137206713+C4LDER4@users.noreply.github.com>
2025-10-15 12:04:44 +0200

    Added the location and the welcome message

--- first-lab/main.py
+++ first-lab/main.py
@@ -1,2 +1,7 @@
 # main.py
-print("Hello, world!")
+print("Hello, world! We are in Luxembourg!")
+
+#print the location
+print("Hotel Legere")
+#print the address
+print("12 Rue du Fort Niedergruenewald, 2226 Luxembourg")
