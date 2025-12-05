de3213887db828fc99372deeb252958a4493faf4
C4LDER4 <n.vilusic@gmail.com>
2025-12-05 01:35:14 +0100

    #Extra lines added to the main

--- first-lab/main.py
+++ first-lab/main.py
@@ -5,3 +5,14 @@ print("Hello, world! We are in Luxembourg!")
 print("Hotel Legere")
 #print the address
 print("12 Rue du Fort Niedergruenewald, 2226 Luxembourg")
+#print the phone number
+print("+352 26 27 28 29")
+#print the website
+print("www.legere.lu")
+#ask for the user's name and greet them
+name = input("Please enter your name: ")
+#greet the user
+print(f"Welcome to Hotel Legere, {name}!")
+#finishing message
+print("We hope you have a pleasant stay with us!")
+print(f"Hope to se you in next TeamOne event, {name}!")
